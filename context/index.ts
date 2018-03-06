6d23511765bc75a9c8bcd78fba783811e930ffd5
Kenneth Lynne <kenneth.lynne@gmail.com>
2018-03-06 17:37:38 +0100

    chore: set up autoformatting

--- context/index.ts
+++ context/index.ts
@@ -1,42 +1,31 @@
-import {IItem} from '../common/alfred-interfaces';
+import { IItem } from '../common/alfred-interfaces';
 
 const alfy = require('alfy');
 import * as shelljs from 'shelljs';
-import {
-  IContext,
-} from './interfaces';
+import { IContext } from './interfaces';
 import '../common/shared';
 
 const filter: string = alfy.input.split(' ')[0];
 
-const items: IItem[] = (
-  shelljs
-    .exec('kubectl config get-contexts --no-headers=true', {silent: true})
-    .stdout as string
-)
+const items: IItem[] = (shelljs.exec(
+  'kubectl config get-contexts --no-headers=true',
+  { silent: true },
+).stdout as string)
   .split('\n')
   .filter(line => line && line.length > 0)
-  .reduce(
-    (items: IContext[], listItem: string) => {
+  .reduce((items: IContext[], listItem: string) => {
+    const matches: RegExpMatchArray = listItem.match(/[\w]+/);
 
-      const matches: RegExpMatchArray = listItem.match(/[\w]+/);
+    if (matches && matches.length > 0) {
+      const contextName: string = listItem.match(/[\w]+/).pop();
 
-      if (matches && matches.length > 0) {
-        const contextName: string = listItem.match(/[\w]+/).pop();
-
-        items
-          .push(
-            {
-              current: listItem.indexOf('*') === 0,
-              name: contextName,
-            },
-          );
-      }
-      return items;
-
-    },
-    [],
-  )
+      items.push({
+        current: listItem.indexOf('*') === 0,
+        name: contextName,
+      });
+    }
+    return items;
+  }, [])
   .filter(item => item.name.match(filter))
   .map((context: IContext): IItem => ({
     arg: context.name,
@@ -45,7 +34,4 @@ const items: IItem[] = (
     valid: true,
   }));
 
-alfy
-  .output(
-    items,
-  );
+alfy.output(items);
--- portforward/index.ts
+++ portforward/index.ts
@@ -1,49 +1,39 @@
-import {
-  IContainer,
-  IPod,
-  IPort,
-} from './interfaces';
+import { IContainer, IPod, IPort } from './interfaces';
 
 const alfy = require('alfy');
 import * as shelljs from 'shelljs';
 import '../common/shared';
-import {
-  IItem,
-} from '../common/alfred-interfaces';
+import { IItem } from '../common/alfred-interfaces';
 
 const filter: string = alfy.input.split(' ')[0];
 let mountPort: string = alfy.input.split(' ')[1] || '1337';
 
-const items: IItem[] = JSON.parse((
-  shelljs
-    .exec('kubectl get pods -o json', {silent: true})
-    .stdout as string
-)).items
-  .reduce(
-    (items: IItem[], pod: IPod) => {
-      let newItems: IItem[] = [];
+const items: IItem[] = JSON.parse(shelljs.exec('kubectl get pods -o json', {
+  silent: true,
+}).stdout as string)
+  .items.reduce((items: IItem[], pod: IPod) => {
+    let newItems: IItem[] = [];
 
-      pod.spec.containers.forEach((container: IContainer) => {
-        if (container.ports) {
-          container.ports.forEach((port: IPort) => {
-            newItems.push({
-              arg: `${pod.metadata.name} ${mountPort}:${port.containerPort}`,
-              autocomplete: `${container.name}:${port.containerPort} ${mountPort}`,
-              subtitle: `${pod.status.phase} ${container.image} on port ${port.containerPort} on ${pod.metadata.name}`,
-              title: `${container.name}:${port.containerPort}`,
-              valid: !!mountPort,
-            });
+    pod.spec.containers.forEach((container: IContainer) => {
+      if (container.ports) {
+        container.ports.forEach((port: IPort) => {
+          newItems.push({
+            arg: `${pod.metadata.name} ${mountPort}:${port.containerPort}`,
+            autocomplete: `${container.name}:${
+              port.containerPort
+            } ${mountPort}`,
+            subtitle: `${pod.status.phase} ${container.image} on port ${
+              port.containerPort
+            } on ${pod.metadata.name}`,
+            title: `${container.name}:${port.containerPort}`,
+            valid: !!mountPort,
           });
-        }
-      });
+        });
+      }
+    });
 
-      return items.concat(newItems);
-    },
-    [],
-  )
+    return items.concat(newItems);
+  }, [])
   .filter(item => item.title.match(filter) || item.subtitle.match(filter));
 
-alfy
-  .output(
-    items,
-  );
+alfy.output(items);
